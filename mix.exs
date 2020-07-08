27a3c2250824907a10cc51f4c2f35b61614e0c95
Tomoki Odaka <tomoki.odaka@aktsk.jp>
2020-07-08 17:03:14 +0900

    Use elixir_uuid

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule Cream.Mixfile do
   defp deps do
     [
       {:memcachex, ">= 0.4.0"},
-      {:uuid, "~> 1.1"},
+      {:elixir_uuid, "~> 1.2.1"},
       {:poison, ">= 2.0.0"},
       {:poolboy, "~> 1.5"},
       {:ex_doc, "~> 0.0", only: :dev},
